44adbfd21515e4081c6f7129710ec12d9eeeced1
AndreGregoletto <argregoletto@gmail.com>
2023-02-11 19:41:38 -0300

    finish filter

--- app/Services/AddCarServices.php
+++ app/Services/AddCarServices.php
@@ -10,14 +10,41 @@ class AddCarServices
 {
     public function index()
     {
-        $cars    = Car::whereStatus(1)->skip($_POST['pag'])->take(10)->with('category', 'brand')->get();
-        $paginas = count(Car::whereStatus(1)->with('category', 'brand')->get());
-        $data = [
-            'cars'   => $cars,
-            'paginas' => $paginas
+        $param = [
+            'pag'      => $_POST['pag'],
+            'name'     => isset($_POST['name'])     ? $_POST['name']     : '',
+            'category' => isset($_POST['category']) ? $_POST['category'] : '',
+            'brand'    => isset($_POST['brand'])    ? $_POST['brand']    : ''
         ];
 
-        return response()->json($data);
+        $cars     = Car::whereStatus(1)->skip($param['pag'])->take(10)->with('category', 'brand');
+        $pags     = Car::whereStatus(1);
+        $response = $this->filter($cars, $pags ,$param);
+
+        return response()->json($response);
+    }
+
+    function filter($cars, $pags, $response)
+    {
+        if(!empty($response['name'])){
+            $cars = $cars->where('name', 'LIKE', '%'.$response['name'].'%');
+            $pags = $pags->where('name', 'LIKE', '%'.$response['name'].'%');
+        }
+
+        if(!empty($response['category'])){
+            $cars = $cars->where('category_id', $response['category']);
+            $pags = $pags->where('category_id', $response['category']);
+        }
+
+        if(!empty($response['brand'])){
+            $cars = $cars->where('brand_id', $response['brand']);
+            $pags = $pags->where('brand_id', $response['brand']);
+        }
+
+        return $response = [
+            'paginas' => count($pags->get()),
+            'cars'    => $cars->get()
+        ];
     }
 
     public function comboBrandCategor()
